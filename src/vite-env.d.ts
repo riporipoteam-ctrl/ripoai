1bfd0bdcedb7efd208a36bea275778bdf32fa0c2
riporipoteam-ctrl <ripo.ripoteam@gmail.com>
2026-06-07 10:20:33 +0200

    Declare model proxy env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_GROQ_API_KEY?: string
+  readonly VITE_OPENROUTER_API_KEY?: string
+  readonly VITE_NVIDIA_BASE?: string
+  readonly VITE_NVIDIA_API_KEY?: string
+  readonly VITE_RIPOAI_PROXY_URL?: string
 }
 
 interface ImportMeta {
